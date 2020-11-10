e382605a4e48a2e59d9df8432bdb59617750850d
Tapio Salonen <tsa.dom@pm.me>
2020-11-10 06:31:15 +0400

    Tehtävien palautus

--- laskarit/viikko2/Unicafe/src/main/java/com/mycompany/unicafe/Paaohjelma.java
+++ laskarit/viikko2/Unicafe/src/main/java/com/mycompany/unicafe/Paaohjelma.java
@@ -5,7 +5,6 @@ public class Paaohjelma {
     public static void main(String[] args) {
         Kassapaate unicafeExactum = new Kassapaate();
         Maksukortti kortti = new Maksukortti(10000);
-        
         unicafeExactum.syoEdullisesti(kortti);
         
         System.out.println( unicafeExactum.edullisiaLounaitaMyyty() );
--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,39 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    @Test
+    public void saldoAlussaOikein(){
+        assertEquals("saldo: 0.10",kortti.toString());
+    }
+    @Test
+    public void rahanLataaminenKasvattaaSaldoaOikein(){
+        kortti.lataaRahaa(54321);
+        assertEquals("saldo: 543.31",kortti.toString());
+    }
+    @Test
+    public void saldoVaheneeOikeinJosRahaaTarpeeksi(){
+        kortti.otaRahaa(5);
+        assertEquals("saldo: 0.5",kortti.toString());
+    }
+    @Test
+    public void saldoEiMuutuJosRahaaEiTarpeeksi(){
+        kortti.otaRahaa(15);
+        assertEquals("saldo: 0.10",kortti.toString());
+    }
+    @Test
+    public void metodiPalauttaaTrueJosRahatRiittavat(){
+        assertTrue(kortti.otaRahaa(5));
+    }
+    @Test
+    public void metoriPalauttaaFalseJosRahatEivätRiita(){
+        assertFalse(kortti.otaRahaa(15));
+    }
+    @Test
+    public void metoriPalauttaaTrueJosRahaaOikeaMaara(){
+        assertTrue(kortti.otaRahaa(10));
+    }
+    @Test
+    public void palauttaakoOikeanSaldon(){
+        assertEquals(10,kortti.saldo());
+    }
 }
